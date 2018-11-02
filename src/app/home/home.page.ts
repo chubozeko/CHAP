c19e9ae9bdadfb76bec4efec1e98e845d652381f
Chubo Zeko <chubozeko@gmail.com>
2018-11-02 16:14:21 +0200

    Created If...else, For, While, Do..while Blocks - Add & Delete;

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -35,7 +35,7 @@ export class HomePage {
     this.branch = document.getElementById("arrow");
     this.branch.addEventListener('click', (e) => this.openSymbolsFAB(e) );
 
-    let branches = document.getElementsByClassName("arrow dropzone");
+    let branches = document.getElementsByClassName("dropzone");
     for(let i=0; i<branches.length; i++){
       branches[i].addEventListener('click', (e) => this.openSymbolsFAB(e) );
     }
@@ -46,7 +46,13 @@ export class HomePage {
 
     // Get the target symbol
     let targetSymbol = event.target || event.srcElement || event.currentTarget;
-    targetSymbol.classList.add('active-symbol');
+
+    if(targetSymbol.id == 's_if_case' || targetSymbol.id == 's_for_loop'
+      || targetSymbol.id == 's_while_loop' || targetSymbol.id == 's_do_while_loop'){
+      targetSymbol.parentElement.classList.add('active-symbol');
+    } else {
+      targetSymbol.classList.add('active-symbol');
+    }
 
     // Create Symbol Options actionsheet
     let options: ActionSheetOptions = {
@@ -56,7 +62,7 @@ export class HomePage {
         {
           text: "Delete Symbol",
           handler: () => {
-            let selectedSymbol = document.getElementsByClassName("symbol active-symbol");
+            let selectedSymbol = document.getElementsByClassName("active-symbol");
             let nextArrow = selectedSymbol[0].nextSibling;
             this.workspace.removeChild(nextArrow);
             selectedSymbol[0].remove();
@@ -77,7 +83,7 @@ export class HomePage {
     // Get symbols FAB
     let symbolsFAB = document.getElementById("symbolsFAB");
     // Get the active arrow/branch
-    let arrows = document.getElementsByClassName("arrow dropzone active-arrow");
+    let arrows = document.getElementsByClassName("dropzone active-arrow");
     // Check if there are other active arrows/branches
     if(arrows.length < 1){
       t.classList.add('active-arrow');
@@ -85,7 +91,7 @@ export class HomePage {
         this.symbolsFAB.activated = true;
       }
     } else {
-      let branches = document.getElementsByClassName("arrow dropzone active-arrow");
+      let branches = document.getElementsByClassName("dropzone active-arrow");
       for(let i=0; i<branches.length; i++){
         branches[i].classList.remove('active-arrow');
       }
@@ -100,10 +106,30 @@ export class HomePage {
 
   public addSymbol(id: string, event){
 
+    let symClass, temp, symbol;
+    if(id == 's_if_case'){
+      symClass = "if_div";
+      temp = document.getElementsByClassName(symClass);
+      symbol = temp[0].cloneNode(true);
+    } else if(id == 's_for_loop'){
+      symClass = "for_div";
+      temp = document.getElementsByClassName(symClass);
+      symbol = temp[0].cloneNode(true);
+    } else if(id == 's_while_loop'){
+      symClass = "while_div";
+      temp = document.getElementsByClassName(symClass);
+      symbol = temp[0].cloneNode(true);
+    } else if(id == 's_do_while_loop'){
+      symClass = "do_while_div";
+      temp = document.getElementsByClassName(symClass);
+      symbol = temp[0].cloneNode(true);
+    } else {
+      temp = document.getElementById(id);
+      symbol = temp.cloneNode(true);
+      symbol.textContent = "";
+    }
+
     // Get and create a new symbol with given id from symbols FAB
-    let temp = document.getElementById(id);
-    let symbol = temp.cloneNode(true);
-    symbol.textContent = "";
 
     // Get the selected arrow/branch to append symbol after
     let branches = document.getElementsByClassName("arrow dropzone active-arrow");
