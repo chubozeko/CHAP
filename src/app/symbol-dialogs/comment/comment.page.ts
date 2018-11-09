5de8a747b606f0549bdfcc4cf901a735be6f155c
Chubo Zeko <chubozeko@gmail.com>
2018-11-09 22:36:26 +0200

    Created all Symbol Dialog Designs

--- src/app/symbol-dialogs/comment/comment.page.ts
+++ src/app/symbol-dialogs/comment/comment.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavParams } from '@ionic/angular';
 
 @Component({
   selector: 'app-comment',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentPage implements OnInit {
 
-  constructor() { }
+  sid;
+  constructor(public modal: ModalController, public navP: NavParams) { 
+    this.sid = navP.get('s_id');
+  }
 
   ngOnInit() {
+  }
+
+  public closeModal(){
+    this.modal.dismiss( this.sid );
   }
 
 }
