1a244d02cc2a0daa84e5e1763a2d8716d36f4558
Chubo Zeko <chubozeko@gmail.com>
2018-11-17 16:08:42 +0200

    Added description and logo to 'About' page;

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -13,7 +13,15 @@ export class AboutPage implements OnInit {
   constructor(public modal: ModalController) { }
 
   ngOnInit() {
-    this.aboutText = "CHAP is a cool flowchart creating program to help newbies understand programming concepts.";
+    this.aboutText = `
+    CHAP is a Flowchart-creating program that allows users to interactively build and run solutions to programming problems. \n
+    Users can solely concentrate on the logical structure of an algorithm, rather than the syntactic format of a typical programming language. \n
+    Additionally, users can generate the corresponding Pseudo Code of their created flowcharts, along with the source code of a few conventional programming languages, such as C++. \n\n
+    Developed by:
+    Chubo Zeko
+    Hasan Tuncel Çoban
+    Penondjira Tjahuha-Kakujaha \n\n
+    EPR402 - Capstone Project - 2018`;
   }
 
   public closeModal(){ this.modal.dismiss(); }
