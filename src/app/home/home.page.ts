01fc42d1037ec2a867799fc274984d76bc7ab9ef
HasanTuncelCoban <45012272+HasanTuncelCoban@users.noreply.github.com>
2022-01-26 16:44:03 +0200

    timerfunction add it on tutorial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1960,22 +1960,43 @@ export class HomePage {
       // }
     }
   }
+ public animateValue(id, start, end, duration) {
+    if (start === end) return;
+    var range = end + start;
+    var current = start;
+    var increment = end > start? 1 : +1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
 
+  
   public startOrPauseExercise() {
     let btn_tut_startExercise = document.getElementById("btn_tut_startExercise");
     if (btn_tut_startExercise.classList.contains('exerciseStarted')) {
+
       // TODO: pause Timer
       btn_tut_startExercise.innerHTML = "Start Exercise";
       btn_tut_startExercise.style.backgroundColor = "green";
       btn_tut_startExercise.classList.remove("exerciseStarted");
+     // let timer =document.getElementById("value");
+  this.animateValue("value", 0, 100, 9000);
+    
     } else {
       // TODO: resume/restart Timer
       btn_tut_startExercise.innerHTML = "Pause Exercise";
       btn_tut_startExercise.style.backgroundColor = "red";
       btn_tut_startExercise.classList.add("exerciseStarted");
     }
-  }
-  
+    
+}
+
   public checkTutorialSolution() {
     // TODO: compare the solutions
     document.getElementById("tut_solutionResultsPanel").style.display = "block";
